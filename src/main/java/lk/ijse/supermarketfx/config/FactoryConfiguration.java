5de3cb8b26b80c0f5ec0e3f823a318a1f141683a
Yenuli Minsadi <samararatney@gmail.com>
2025-08-18 15:06:59 +0530

    Added FactoryConfiguration

---
+++ src/main/java/lk/ijse/supermarketfx/config/FactoryConfiguration.java
@@ -0,0 +1,36 @@
+package lk.ijse.supermarketfx.config;
+
+import lk.ijse.supermarketfx.entity.Customer;
+import lk.ijse.supermarketfx.entity.Item;
+import lk.ijse.supermarketfx.entity.Order;
+import lk.ijse.supermarketfx.entity.OrderDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//Private nisa obj hdnnth ba and instance hdnna ba meke
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration().configure();
+        configuration.configure("hibernate.cfg.xml");//dennm oni file eka read krnna / must give the path file name ek wenas kroth
+        configuration.addAnnotatedClass(Customer.class);
+        configuration.addAnnotatedClass(Item.class);
+        configuration.addAnnotatedClass(Order.class);
+        configuration.addAnnotatedClass(OrderDetail.class);
+
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ?
+                factoryConfiguration = new FactoryConfiguration() :
+                factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module lk.ijse.supermarketfx {
     requires net.sf.jasperreports.core;
     requires java.mail;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
     opens lk.ijse.supermarketfx.controller to javafx.fxml;
     opens lk.ijse.supermarketfx.dto.tm to javafx.base;
