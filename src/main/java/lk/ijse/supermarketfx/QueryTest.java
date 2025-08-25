9d9f921295720cbefa88f394cdd0b88919a3914a
Yenuli Minsadi <samararatney@gmail.com>
2025-08-25 10:06:37 +0530

    HQL Where case with parameter

--- src/main/java/lk/ijse/supermarketfx/QueryTest.java
+++ src/main/java/lk/ijse/supermarketfx/QueryTest.java
@@ -16,19 +16,31 @@ public class QueryTest {
         Session session = factoryConfiguration.getSession();
 
         //HQL / JPQL with Where case-att eke name eken
-        Query<Customer> query = session.createQuery("from Customer where name='Jhon Doe'", Customer.class);//returns a query object, we can take out a list from this
-        List<Customer> list = query.list();
-        list.forEach(customer->{
-            System.out.println(customer.toString());
-        });
+//        Query<Customer> query = session.createQuery("from Customer where name='Jhon Doe'", Customer.class);//returns a query object, we can take out a list from this
+//        List<Customer> list = query.list();
+//        list.forEach(customer->{
+//            System.out.println(customer.toString());
+//        });
 
         //Native SQL with Where clause-tbl column name eken
         NativeQuery<Customer> nativeQuery = session.createNativeQuery(
-                "select * from customers where customer_name='John Doe'", Customer.class);
+                "select * from customers where customer_name:cus_name", Customer.class);
+        nativeQuery.setParameter("cus_name","John Doe");
         List<Customer> customerList = nativeQuery.list();
         customerList.forEach(customer->{
             System.out.println(customer.toString());
         });
+
+        //HQL where case with parameter
+//        session.createQuery("",entity class) - HQL/JPQL ->query and then class name
+//        session.createNativeQuery("",entity class) - SQL
+       Query<Customer> query = session.createQuery("from Customer where name: cus_name",Customer.class);
+       query.setParameter("cus_name","John Doe");
+       List<Customer>list = query.list();
+
+       for (Customer customer : customerList) {
+           System.out.println(customer.toString());
+       }
 
     }
 }
